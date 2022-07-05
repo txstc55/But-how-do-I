d25c6d1fb30745ab1cb58cd32e464aea2d73cbfb
Xuan Tang <txstc55@gmail.com>
2022-07-05 23:26:01 +0800

    style change

--- src/components/PostGridComponent.vue
+++ src/components/PostGridComponent.vue
@@ -5,7 +5,7 @@
       mx-auto shadow-inner
       mb-5
       bg-slate-900/20
-      hover:shadow-2xl animate-all transition hover:-translate-y-1
+      hover:shadow-2xl animate-all transition hover:-translate-y-1 border-gray-900 border-2 rounded-sm hover:bg-slate-700/20 duration-200
     "
   >
     <div class="text-white text-xl sm:text-xl md:text-2xl lg:text-3xl xl:text-3xl break-words px-5 py-7 font-mono">
