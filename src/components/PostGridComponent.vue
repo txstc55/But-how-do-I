3b051f6dbd584e411b52735e0121859b4988034c
Xuan Tang <txstc55@gmail.com>
2022-07-05 23:28:12 +0800

    style change

--- src/components/PostGridComponent.vue
+++ src/components/PostGridComponent.vue
@@ -2,13 +2,34 @@
   <div
     class="
       w-4/5
-      mx-auto shadow-inner
+      mx-auto
+      shadow-inner
       mb-5
       bg-slate-900/20
-      hover:shadow-2xl animate-all transition hover:-translate-y-1 border-gray-900 border-2 rounded-sm hover:bg-slate-700/20 duration-200
+      hover:shadow-2xl
+      animate-all
+      transition
+      hover:-translate-y-1
+      border-gray-900 border-2
+      rounded-sm
+      hover:bg-slate-700/20
+      duration-200
+      hover:border-gray-900/30
     "
   >
-    <div class="text-white text-xl sm:text-xl md:text-2xl lg:text-3xl xl:text-3xl break-words px-5 py-7 font-mono">
+    <div
+      class="
+        text-white text-xl
+        sm:text-xl
+        md:text-2xl
+        lg:text-3xl
+        xl:text-3xl
+        break-words
+        px-5
+        py-7
+        font-mono
+      "
+    >
       <router-link :to="'/post/' + title">{{ title }}</router-link>
     </div>
     <TagComponent :tags="tags"></TagComponent>
