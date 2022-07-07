d92adf9ec5e788dbf0059896e3a7466174c54e50
Xuan Tang <txstc55@gmail.com>
2022-07-07 22:08:17 +0800

    add style

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,20 @@
 <template>
   <div>
-    <TitleComponent />
+    <div class="h-screen w-screen"></div>
+    <div
+      class="fixed top-0 h-screen w-screen main-title"
+      :style="{ filter: 'blur(' + this.blur + 'px)', opacity: this.opacity }"
+    >
+      <TitleComponent />
+    </div>
+    <div :style="{ height: '50vh' }"></div>
     <PostGridView></PostGridView>
     <div class="mt-1 mb-4 text-gray-400 w-full text-center">
       A collection of code snippets by
-      <a target="_blank" href="https://txstc55.github.io/" class="underline italic"
+      <a
+        target="_blank"
+        href="https://txstc55.github.io/"
+        class="underline italic"
         >Xuan Tang</a
       >
       from his side projects<br />Suggestions? Email me:
@@ -25,13 +35,40 @@ export default {
     PostGridView,
   },
   data() {
-    return {};
+    return {
+      blur: 0,
+      opacity: 1,
+    };
+  },
+  methods: {
+    computeBlurAndOpacity() {
+      this.blur = Math.min(
+        15,
+        Math.ceil((window.scrollY / (window.innerHeight * 0.7)) * 15)
+      );
+      this.opacity = Math.max(
+        0,
+        1.5 - window.scrollY / (window.innerHeight * 0.7)
+      );
+    },
+    scrollMethod() {
+      this.computeBlurAndOpacity();
+    },
+    resizeMethod() {
+      this.computeBlurAndOpacity();
+    },
   },
   created() {
     setTimeout(() => {
       // scroll to top when animation is finished
       window.scrollTo(0, 0);
     }, 510);
+    window.addEventListener("scroll", this.scrollMethod);
+    window.addEventListener("resize", this.resizeMethod);
+  },
+  destroyed() {
+    window.removeEventListener("scroll", this.scrollMethod);
+    window.removeEventListener("resize", this.resizeMethod);
   },
 };
 </script>
